f62fe061a65072269ac16f13a83defdb23426467
kessie <kayp3477@gmail.com>
2024-03-25 01:22:32 +0000

    Add views, js and css resources

---
+++ resources/views/terms.blade.php
@@ -0,0 +1,15 @@
+@extends('layout.layout')
+
+@section('content')
+    <h1>Terms</h1>
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque eveniet sed voluptate repellat expedita
+        perspiciatis autem eligendi laborum reiciendis nemo libero commodi, assumenda corrupti sint natus quaerat a
+        maiores doloribus repudiandae voluptates aliquid est sequi quisquam nisi. Laudantium molestias quibusdam
+        quaerat nulla illum placeat cupiditate, at maiores praesentium et odit quod qui ab dolore consequatur
+        libero. Laudantium, excepturi numquam esse rerum culpa perspiciatis ipsa vel sequi facilis nemo reiciendis
+        modi minus aperiam ut dolorem. Nesciunt, totam officiis culpa, libero rem eligendi, animi natus inventore
+        expedita adipisci voluptatum. Minima minus eligendi unde, deleniti incidunt nobis beatae. Earum aliquam
+        incidunt aperiam odio?
+    </div>
+@endsection
