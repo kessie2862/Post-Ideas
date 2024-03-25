f38910abbc5f404f046bea7329ecdabc2a2b834a
kessie <kayp3477@gmail.com>
2024-03-25 11:59:35 +0000

    Add form action

--- resources/views/shared/submit-idea.blade.php
+++ resources/views/shared/submit-idea.blade.php
@@ -1,9 +1,13 @@
 <h4> Share yours ideas </h4>
 <div class="row">
-    <div class="mb-3">
-        <textarea class="form-control" id="idea" rows="3"></textarea>
-    </div>
-    <div class="">
-        <button class="btn btn-dark"> Share </button>
-    </div>
+    {{-- Using named route in form action --}}
+    <form action="{{ route('idea.create') }}" method="post">
+        @csrf
+        <div class="mb-3">
+            <textarea name="idea" class="form-control" id="idea" rows="3"></textarea>
+        </div>
+        <div class="">
+            <button type="submit" class="btn btn-dark"> Share </button>
+        </div>
+    </form>
 </div>
