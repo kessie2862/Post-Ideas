614be10b3512b91bc20ae0c07d42cc081c137f3b
kessie <kayp3477@gmail.com>
2024-03-25 10:56:02 +0000

    Add error message shared file

---
+++ resources/views/shared/error-message.blade.php
@@ -0,0 +1,4 @@
+<div class="alert alert-danger alert-dismissible fade show" role="alert">
+    Error Creating Idea.
+    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+</div>
