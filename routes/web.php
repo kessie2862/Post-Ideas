185ddd2185109e491483ccfaac79227a411d4f7d
kessie <kayp3477@gmail.com>
2024-03-25 11:55:49 +0000

    Add named route for idea controller

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,14 @@
 <?php
 
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\IdeaController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 // Whenever you get a request for the root URL ('/), Laravel will call the index method of the DashboardController class.
-Route::get('/', [DashboardController::class, 'index']);
+Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+
+Route::post('/idea', [IdeaController::class, 'store'])->name('idea.create'); // Using named routes
 
 Route::get('/terms', function () {
     return view('terms');
