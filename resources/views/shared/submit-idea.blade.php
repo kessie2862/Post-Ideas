a5fc7522103a9ddf18be2db25304135f3c88dd9f
kessie <kayp3477@gmail.com>
2024-03-26 03:34:23 +0000

    Display form validation error messages

--- resources/views/shared/submit-idea.blade.php
+++ resources/views/shared/submit-idea.blade.php
@@ -5,6 +5,11 @@
         @csrf
         <div class="mb-3">
             <textarea name="idea" class="form-control" id="idea" rows="3"></textarea>
+            @error('idea')
+                <span class="text-danger fs-6 d-block mt-2">
+                    {{ $message }}
+                </span>
+            @enderror
         </div>
         <div class="">
             <button type="submit" class="btn btn-dark"> Share </button>
