0d3b72f45ea8d5d4b6c6614e09b1a20209d7992e
kessie <kayp3477@gmail.com>
2024-03-28 02:13:24 +0000

    Changed route in form action from create to store

--- resources/views/shared/submit-idea.blade.php
+++ resources/views/shared/submit-idea.blade.php
@@ -1,7 +1,7 @@
 <h4> Share yours ideas </h4>
 <div class="row">
     {{-- Using named route in form action --}}
-    <form action="{{ route('idea.create') }}" method="post">
+    <form action="{{ route('idea.store') }}" method="post">
         @csrf
         <div class="mb-3">
             <textarea name="idea" class="form-control" id="idea" rows="3"></textarea>
