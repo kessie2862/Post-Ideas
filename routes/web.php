ef046babbeb38898d7ede7a257b3cbb028f3f8ab
kessie <kayp3477@gmail.com>
2024-03-28 02:37:26 +0000

    Add route for showing idea

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,8 @@ use Illuminate\Support\Facades\Route;
 // Whenever you get a request for the root URL ('/), Laravel will call the index method of the DashboardController class.
 Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
 
+Route::get('/ideas/{idea}', [IdeaController::class, 'show'])->name('ideas.show');
+
 Route::post('/ideas', [IdeaController::class, 'store'])->name('idea.store'); // Using named routes
 
 Route::delete('/ideas/{id}', [IdeaController::class, 'destroy'])->name('ideas.destroy');
